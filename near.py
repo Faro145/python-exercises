17ed656c1854b46fd826011765dd48cd3ad276db
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-10-23 14:00:29 +0100

    Update near.py

--- near.py
+++ near.py
@@ -1,11 +1,14 @@
 def near (string1, string2):
 
-    a = len(string1)
+    a = len(string1) - 1
     b = len(string2)
 
-    if a > b:
+    if a == b:
         return True
     else:
         return False
 
 print(near("reset", "rest"))
+print(near("dragoon", "dragon"))
+print(near("eave", "leave"))
+print(near("sleet", "lets"))
