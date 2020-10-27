60d496fbf5e44985597a819cc701d1e034bcb97b
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-10-27 16:16:39 +0000

    maze part of maze_turner.py

---
+++ programs/maze_turner.py
@@ -0,0 +1,9 @@
+print("Maze Generator")
+print("Help: # = wall, E = exit,  = passage, > = explorer")
+
+line_one = list(input("Enter top line of maze: "))
+line_two = list(input("Enter second line of maze: "))
+line_three = list(input("Enter second line of maze: "))
+
+print("Your maze:")
+print(line_one, line_two, line_three, sep="\n")
