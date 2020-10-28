c21bc89ecd55a2b2e686c9059603f0b2f2f8dd7a
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-10-28 12:21:14 +0000

    Update near.py

--- programs/near.py
+++ programs/near.py
@@ -1,14 +1,15 @@
 def near (string1, string2):
+    list_string1 = list(string1)
+    list_string2 = list(string2)
+    for index in range(len(list_string1)):
+        item_to_remove = list_string1[index]
+        del list_string1[index]
+        if list_string1 == list_string2:
+            return True
+        list_string1.insert(index, item_to_remove)
+    return False
+    
+word1 = input("Enter First Word: ")
+word2 = input("Enter Second Word: ")
 
-    a = len(string1) - 1
-    b = len(string2)
-
-    if a == b:
-        return True
-    else:
-        return False
-
-print(near("reset", "rest"))
-print(near("dragoon", "dragon"))
-print(near("eave", "leave"))
-print(near("sleet", "lets"))
+print(near(word1, word2))
