7c7b31da1d2e170ee00f7a52b304b2145190e995
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-10-28 12:55:21 +0000

    Update maze_turner.py

--- programs/maze_turner.py
+++ programs/maze_turner.py
@@ -7,3 +7,10 @@ line_three = list(input("Enter second line of maze: "))
 
 print("Your maze:")
 print(line_one, line_two, line_three, sep="\n")
+
+line_two[1], line_two[4] = line_two[4], line_two[1]
+
+if line_two[1] == "E":
+    print("Escaped")
+else:
+    print("Trapped")
