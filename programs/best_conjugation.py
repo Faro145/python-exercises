62f0f38e2f289d6a253e7f53f5d20257bd2b7e5d
Faro145 <rossfarq@gmail.com>
2020-10-29 15:20:03 +0000

    update best_conjugation.py

--- programs/best_conjugation.py
+++ programs/best_conjugation.py
@@ -1,4 +1,22 @@
-file_name = 'wordlist.txt'
-entrance = open(file_name)
-wordlist = entrance.readlines()
-print(wordlist)
+import random
+
+def random_line(fname):
+    lines = open(fname).read().splitlines()
+    return random.choice(lines)
+    
+word = random_line('wordlist.txt')
+
+letters = list(word)
+
+print(word)
+
+if len(word) == 9:
+    subword_1 = ''.join(letters[0:3])
+    subword_2 = ''.join(letters[3:6])
+    subword_3 = ''.join(letters[6:9])
+    print(subword_1)
+    print(subword_2)
+    print(subword_3)
+else:
+    print("Try Again")
+
