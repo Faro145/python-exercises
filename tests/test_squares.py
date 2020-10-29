7e78246aee8099734ab81e62f0a95f0f9af2dda9
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-10-29 11:25:32 +0000

    Update test_squares.py

--- tests/test_squares.py
+++ tests/test_squares.py
@@ -1,5 +1,5 @@
 import pytest
-import list_of_squares
+from programs import list_of_squares
 
 def test_listofsquares_six():
     assert list_of_squares.list_of_squares(6) == {1:1, 2:4, 3:9, 4:16, 5:25, 6:36}
